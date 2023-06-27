43658d50c3eb4df6788a9eb9fe2a27cd152790c8
PP Pixel <134684159+PP-Pixel@users.noreply.github.com>
2023-06-27 15:09:21 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,37 +29,6 @@ workbox.routing.registerRoute(
   new RegExp('index.html'),
   new workbox.strategies.NetworkFirst()
 );
-workbox.routing.registerRoute(
-  new RegExp('activity.html'),
-  new workbox.strategies.NetworkFirst()
-);
-workbox.routing.registerRoute(
-  new RegExp('company.html'),
-  new workbox.strategies.NetworkFirst()
-);
-workbox.routing.registerRoute(
-  new RegExp('contact.html'),
-  new workbox.strategies.NetworkFirst()
-);
-workbox.routing.registerRoute(
-  new RegExp('faq.html'),
-  new workbox.strategies.NetworkFirst()
-);
-workbox.routing.registerRoute(
-  new RegExp('404.html'),
-  new workbox.strategies.NetworkFirst()
-);
-workbox.routing.registerRoute(
-  new RegExp('http://4d3.gmobb.jp/dcm8wdyx8bibu/cgi/joyful/joyful.cgi'),
-  new workbox.strategies.NetworkFirst()
-);
-//font
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New&display=swap'),
-  new workbox.strategies.NetworkFirst()
-);
-
-
 
 // manifest
 workbox.routing.registerRoute(
@@ -67,41 +36,23 @@ workbox.routing.registerRoute(
   new workbox.strategies.StaleWhileRevalidate()
 );
 workbox.routing.registerRoute(
-  new RegExp('https://pp-pixel.github.io/csclub/manifest.json'),
+  new RegExp('https://pp-pixel.github.io/clock3/manifest.json'),
   new workbox.strategies.StaleWhileRevalidate()
 );
 
 // ------------------  precaching the assets ---------------------
   const OFFLINE_PAGE = 'index.html';
   workbox.precaching.precacheAndRoute([
-    //404
-    '404.html',
-    //images
-    'activity-room.png',
-    'logo.png',
-    'logo.psd',
-    'logo.webp',
-    'mainimg_simple.mp4',
     //icon,ogp
-    'favicon.ico',
-    'favicon.png',
-    'icon-192.png',
-    'icon-256.png',
-    'ogp-mage.png',
-    'ogp-mage.webp',
+    'TIME.ico',
     //css
     'style.css',
-    'animation.css',
-    'inview.css',
+    'index.css',
     //javaScript
+    'js',
     'main.js',
-    'jquery.inview_set.js',
-    'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js',
     'https://www.google.com/recaptcha/api.jp',
     'https://www.google.com/recaptcha/enterprise.js?render=6LeoDWgmAAAAAGwPsVvUHB8UzQsZj-w6wxO_Ccxl'.
-    //pdf
-    'r5-recruitment.pdf',
     //other
     'manifest.json',
    OFFLINE_PAGE,  
